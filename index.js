511bb8d325d946b65ef012923cda525ace0984b6
Italo <igbehh@gmail.com>
2022-06-07 19:33:25 -0300

    feat: initial page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./database/database");
 const app = express();
+const Pergunta = require('./database/Pergunta');
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -12,48 +13,60 @@ app.get("/perguntar", (req, res) => {
     res.render('perguntar');
 });
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/", async (req, res) => {
+
+    var response = await Pergunta.findAll({
+        raw:true
+    });
+
+    res.render("index", {
+        data : response
+    });
 });
 
-app.post("/salvarpergunta", (req, res) => {
+app.post("/salvarpergunta", async (req, res) => {
     var title = req.body.title;
     var value = req.body.value;
 
-    res.send(title + " > " + value);
+    await Pergunta.create({
+        titulo: title,
+        descricao: value
+    });
+
+    res.redirect("/");
 
 });
 
-app.get("/mysql", async (req, res) => {
-    try {
-        await db.authenticate();
-        console.log('Connection has been established successfully.');
-        const result = await db.query("SELECT * FROM PERGUNTAS");
-        res.send(result[0]);
+// app.get("/mysql", async (req, res) => {
+//     try {
+//         await db.authenticate();
+//         console.log('Connection has been established successfully.');
+//         const result = await db.query("SELECT * FROM PERGUNTAS");
+//         res.send(result[0]);
 
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-
-
-});
-
-app.post("/mysql", async (req, res) => {
-    if (!req.body.title || !req.body.texto) {
-        res.send("Erro");
-    }
-
-    let title = req.body.title;
-    let texto = req.body.texto;
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//     }
 
 
-    await db.query("INSERT INTO perguntas (title,texto) values ( '" +
-        title + "' , '" +
-        texto + "'" + ")"
+// });
 
-    ), { type: db.QueryTypes.INSERT };
-    res.end();
-});
+// app.post("/mysql", async (req, res) => {
+//     if (!req.body.title || !req.body.texto) {
+//         res.send("Erro");
+//     }
+
+//     let title = req.body.title;
+//     let texto = req.body.texto;
+
+
+//     await db.query("INSERT INTO perguntas (title,texto) values ( '" +
+//         title + "' , '" +
+//         texto + "'" + ")"
+
+//     ), { type: db.QueryTypes.INSERT };
+//     res.end();
+// });
 
 app.listen(3000, () => {
     console.log("Sucesso");
